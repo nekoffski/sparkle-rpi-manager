99eba394105214a07d1c09ae8147cffe8750d7a6
nekoffski <kacperczyz@protonmail.com>
2021-12-13 21:08:08 +0100

    fix typo

--- bin/update.sh
+++ bin/update.sh
@@ -5,7 +5,7 @@ pushd ~
 git clone https://github.com/nekoffski/sparkle-rpi-manager.git --single-branch --branch develop srpi
 systemctl stop sparkle-rpi-manager
 cp ./srpi/src/rpi-manager/* /usr/local/sparkle-rpi-manager/src/rpi-manager -rf
-rm srp -rf
+rm srpi -rf
 
 popd
 

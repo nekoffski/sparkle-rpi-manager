d0cd0693fbcb92d655dd253b41b63919d31e3b27
nekoffski <kacperczyz@protonmail.com>
2021-12-13 21:21:55 +0100

    fix

--- bin/update.sh
+++ bin/update.sh
@@ -5,6 +5,8 @@ pushd ~
 git clone https://github.com/nekoffski/sparkle-rpi-manager.git --single-branch --branch develop srpi
 systemctl stop sparkle-rpi-manager
 cp ./srpi/src/rpi-manager/* /usr/local/sparkle-rpi-manager/src/rpi-manager -rf
+cp ./srpi/version /usr/local/sparkle-rpi-manager/ -f
+
 rm srpi -rf
 
 popd
